c4ba9aecd896ccbd0188e7261d2c9c807a8f2821
Mohamed Hassan <119795945+MohamedElsawsany@users.noreply.github.com>
2025-05-20 02:01:28 +0300

    Change_Navbar_Link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ export default function Header() {
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <Link className="nav-link" aria-current="page" to="/products">
-                  Recpices
+                  Products
                 </Link>
               </li>
               <li className="nav-item">
